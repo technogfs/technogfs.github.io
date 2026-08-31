314c17d11057a71f166edb005c90026e3d7b1ddc
Ali Honarmand <honarmand.ali.ha@gmail.com>
2026-08-31 07:19:22 +0330

    feat: testing github pages build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: ""
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
